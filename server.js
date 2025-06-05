3db6778ee0ebcf9e90ef4f014f862f1c09d1ed5d
altynay <goldmoon321123@gmail.com>
2025-06-05 19:58:37 +0500

    add hashing

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const {ObjectID } = require('mongodb');
 const app = express();
 const LocalStrategy = require('passport-local');
+const hash = bcrypt.hashSync(req.body.password, 12);
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
@@ -33,7 +34,9 @@ myDB(async client => {
       console.log(`User ${username} attempted to log in.`);
       if (err) return done(err);
       if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false);
+      }
       return done(null, user);
     });
   }));
@@ -70,7 +73,7 @@ myDB(async client => {
               } else {
                 myDataBase.insertOne({
                       username: req.body.username,
-                      password: req.body.password
+                      password: hash
                     },
                     (err, doc) => {
                       if (err) {
