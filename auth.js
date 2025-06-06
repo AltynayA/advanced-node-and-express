8a14aaa55067d1db58283f125c16380f24587b27
altynay <goldmoon321123@gmail.com>
2025-06-06 18:38:38 +0500

    fix syntax error in auth.js

--- auth.js
+++ auth.js
@@ -28,7 +28,7 @@ module.exports = function (app, myDataBase) {
     passport.use(new GitHubStrategy({
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: 'https://advanced-node-and-express-v3nh.onrender.com/auth/github/callback';
+            callbackURL: 'https://advanced-node-and-express-v3nh.onrender.com/auth/github/callback'
         },
         function(accessToken, refreshToken, profile, cb) {
             console.log(profile);
