81f9f2b5965fc7d0da2a45eae42efea6750fac71
altynay <goldmoon321123@gmail.com>
2025-06-08 22:14:00 +0500

    add authentication with web socket

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const auth = require('./auth.js');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
@@ -27,6 +33,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+);
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   routes(app, myDataBase);
@@ -35,8 +52,8 @@ myDB(async client => {
   io.on('connection', socket => {
     ++currentUsers;
     io.emit('user count', currentUsers);
-    console.log('A user has connected');
-
+    // console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
@@ -48,6 +65,18 @@ myDB(async client => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
